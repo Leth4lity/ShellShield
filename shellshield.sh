8be8ec4d505b2753a070f86d9b707a68500047cb
Laken J Brion <60785066+Leth4lity@users.noreply.github.com>
2022-06-13 01:39:08 -0400

    shellshield.sh

--- shellshield.sh
+++ shellshield.sh
@@ -1,9 +1,28 @@
+echo "    ";
+echo "    ";
 echo "Welcome to Shellshield!";
+echo "    ";
+echo "    ";
 echo "created by Leth4lity.";
-echo "";
+echo "    ";
 echo "The Guardian of the Open Internet."
+echo "    ";
+echo "    ";
 echo "Login: ";
 read $USER;
-echo "One-Time Passcode: ";
-read $PASS;
-echo "Initializing $USER...";
+
+# Password Management
+
+#echo "One-Time Passcode: ";
+#read $PASS;
+
+echo "Initializing" $USER"...";
+echo "    ";
+echo "    ";
+echo "<< Starting up Cyber-Armor-VPN™ >>";
+echo "----------------------------------";
+echo "||||||||||||||||||||||||||||||||||";
+echo "✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓";
+
+# Insert openvpn/protonvpn command;
+echo "Protection is active!";
