24624596bb599d6cb043f861712c2b8b433582d1
Laken J Brion <60785066+Leth4lity@users.noreply.github.com>
2022-06-12 23:01:09 -0400

    Add files via upload

---
+++ shellshield.sh
@@ -0,0 +1,9 @@
+echo "Welcome to Shellshield!";
+echo "created by Leth4lity.";
+echo "";
+echo "The Guardian of the Open Internet."
+echo "Login: ";
+read $USER;
+echo "One-Time Passcode: ";
+read $PASS;
+echo "Initializing $USER...";
