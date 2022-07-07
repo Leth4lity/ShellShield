373b5ffbb4656025868afd8a793b36e889a17c39
Laken J Brion <60785066+Leth4lity@users.noreply.github.com>
2022-07-07 09:26:17 -0500

    Added proxy service via getproxy.
    Need to create a script that asks the  user if they want their connection tunneled via proxy services; and if they do choose to tunnel, then we must format the proxys to coincide with proxychains4.conf.

---
+++ getproxy.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o socks5-proxys.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o socks4-proxys.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o http-proxys.txt;
+
